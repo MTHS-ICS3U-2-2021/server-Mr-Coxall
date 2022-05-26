18499cdc146cc7edaa8e80246f8044ae18b6a1f0
Patrick Coxall <mr.coxall@mths.ca>
2022-05-26 12:54:06 +0000

    update

--- ICS3U/helloWorld.cpp
+++ ICS3U/helloWorld.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-  
-int main() {
-    std::cout << "Hello, World!";
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
